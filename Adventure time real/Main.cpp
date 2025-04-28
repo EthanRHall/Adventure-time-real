778f49bd48f6fe19c8b1e54e4a23df5a4fc6f559
Ethan Hall <erh5512@psu.edu>
2025-04-28 18:15:48 -0400

    Add project files.

---
+++ Adventure time real/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+
+int main() {
+
+
+	cout << "Good morning! Today is the day, the day you will become something more than yourself...more than what you've ever thought you could be and more than you could ever hope to become." << endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
+	cout << "Honestly, I never thought you'd get to this point. You, Frank, and your dog, Jim, have grown to become a duet that I couldn't be more proud of. No matter what happens today, I will always have that pride." << endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
+	cout << "Well, the time has come. Will you set off?" << endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
+	cout << "(when giving answers, please do not use caps or punctuation of any kind.)";
+
+	//insert loop where if yes then the game begins and if no then the game ends.
+
+	cout << "You and Jim walk from your house towards the woods. You've never been before, but in your time here you've always wondered what lay within." << endl;
+
+	cout << "You're not really sure how you got here. In your home there's never been any parents, a concept of which you know nothing about, but the idea that there should be someone to care for you has always been at the back of your mind." << endl;
+
+	cout << "You have never seen a living thing other than Jim. You and him have been together for a long time, but just as you're unsure of your own heritage you are also unsure of his." << endl;
+
+	cout << "Every morning you had woken up, rolled out of bed, and everything you needed was just...there. Cooked meals, water, clothes, you've needed for nothing. And everything just seems to manifest when you're not looking." << endl;
+
+	cout << "Come to think of it, who was that talking to you this morning? Never in your life have you heard spoken words, yet, you understood the voice perfectly. You felt safe..." << endl;
+
+	cout << "** As you needlessly ponder your existence, you are approached by some sort of bird.**" << endl;
+
+	cout << "Bird thing: Good morning traveller and dog! I see you're trying to walk through MY territory." << endl;
+
+	cout << "While it is true this is MINE, I am feeling generous on this beautiful morning the forest has been blessed with. I will let you pass if you can answer one simple question, matter of fact if you answer it correctly I'll keep myself from ending you and your dog!" << endl;
+
+	//insert the first math/science question here. 5 questions based on those subjects with a random 1 in 5 chance to get each one when the interaction begins
+
+// test test 
+
+	return 0;
+}
---
+++ Adventure time real/Main_events.cpp
@@ -0,0 +1,2 @@
+#include "Main_events.h"
+using namespace std;
